d7d4fa13d39e8d221d19451277f7c261dc35badb
matheusgomesslv222 <matheusgomesslv222@gmail.com>
2024-08-21 06:58:37 -0300

    update

--- bit_project/bit_viewer/models.py
+++ bit_project/bit_viewer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
