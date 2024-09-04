ea3f295395b4696a6d9302abfc390cae186bda38
Guilherme <guilhermemspiandorin@gmail.com>
2024-09-04 10:08:56 -0300

    fix: Adição dos Models no `admin.py`

--- bit_project/bit_viewer/admin.py
+++ bit_project/bit_viewer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UsuarioInfo, Pool, Maquina
+
+admin.site.register(UsuarioInfo)
+admin.site.register(Pool)
+admin.site.register(Maquina)
 
 # Register your models here.
