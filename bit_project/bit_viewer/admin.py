e26982495ddec3035298ac4f4f59116eab05ebee
Matheus Gomes da Silva <101715170+matheusgomesslv222@users.noreply.github.com>
2024-09-11 07:37:21 -0300

    Delete bit_project directory

--- bit_project/bit_viewer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UsuarioInfo, Pool, Maquina
-
-admin.site.register(UsuarioInfo)
-admin.site.register(Pool)
-admin.site.register(Maquina)
-
-# Register your models here.
--- bit_project/bit_viewer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BitViewerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bit_viewer'
